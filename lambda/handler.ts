d16031947dea5acfab24a4d38579c13651b0d390
Steve Sanders <sdsanders1@gmail.com>
2024-12-04 09:20:17 -0600

    connect via drizzle

--- lambda/handler.ts
+++ lambda/handler.ts
@@ -1,5 +1,6 @@
 import { DsqlSigner } from '@aws-sdk/dsql-signer';
 import { Client } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
 
 export const handler = async () => {
   const clusterEndpoint = 'oqabtu7fln6kh65nvyj62cbh3e.dsql.us-east-1.on.aws';
@@ -20,30 +21,8 @@ export const handler = async () => {
       port: 5432,
       ssl: true,
     });
-
-    // Connect
-    await client.connect();
-
-    // Create a new table
-    await client.query(`CREATE TABLE IF NOT EXISTS owner (
-      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-      name VARCHAR(30) NOT NULL,
-      city VARCHAR(80) NOT NULL,
-      telephone VARCHAR(20)
-    )`);
-
-    // Insert some data
-    await client.query(
-      'INSERT INTO owner(name, city, telephone) VALUES($1, $2, $3)',
-      ['John Doe', 'Anytown', '555-555-1900']
-    );
-
-    // Check that data is inserted by reading it back
-    const result = await client.query(
-      "SELECT id, city FROM owner where name='John Doe'"
-    );
-
-    await client.query("DELETE FROM owner where name='John Doe'");
+    const db = drizzle({ client });
+    const result = await db.execute('select 1');
 
     return result;
   } catch (error) {
